1b45e8c4917fea6af2bdadb4d93267486a323472
Bao Nguyen <bao.na30@gmail.com>
2023-03-29 18:20:48 -0700

    Android App

--- gitpush.bat
+++ gitpush.bat
@@ -1,4 +1,11 @@
 cd C:\data_2023\Hoc\Bao\Projects\Water Reminder
+
+git reset
+git remote -v
+git pull --rebase
+git pull origin WaterReminder
+git branch --set-upstream-to=WaterReminder
+
 git init
 git add .
 git commit -m "Android App"
