f42522764fbed66e5c081bb9af64db88378cf545
Bao Nguyen <100505220+baona30@users.noreply.github.com>
2023-03-29 18:52:14 -0700

    Delete git_push.bat

--- git_push.bat
+++
@@ -1,6 +0,0 @@
-cd c:\data_2023\hoc\bao\projects\water reminder
-git init -b main
-git remote add origin https://github.com/baona30/baona30.git
-git add .
-git commit -m "Add Android App"
-git push -u origin main
